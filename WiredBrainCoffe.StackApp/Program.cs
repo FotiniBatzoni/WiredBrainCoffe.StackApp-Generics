f952b0bea4a23f55182ccb18f00a6089129196b1
fotini <f.batzoni@gmail.com>
2023-03-26 19:29:35 +0300

    Using copy-paste method

--- WiredBrainCoffe.StackApp/Program.cs
+++ WiredBrainCoffe.StackApp/Program.cs
@@ -1,7 +1,15 @@
 ﻿
 
+StackDoubles();
+StackStrings();
 
-    var stack = new SimpleStack();
+Console.ReadLine();
+
+
+
+static void StackDoubles()
+{
+    var stack = new SimpleStackDouble();
 
     stack.Push(1.2);
     stack.Push(2.8);
@@ -11,13 +19,23 @@
 
     while (stack.Count > 0)
     {
-        double item = stack.Pop();
+        double item = (double)stack.Pop();
         Console.WriteLine($"Item: {item}");
         sum += item;
     }
 
-    Console.WriteLine($"Sum: {sum}" );
+    Console.WriteLine($"Sum: {sum}");
+}
 
-    Console.ReadLine();
+void StackStrings()
+{
+    var stack = new SimpleStackString();
+    stack.Push("Wired Brain Coffee");
+    stack.Push("Pluralsight");
 
-
+    while (stack.Count > 0)
+    {
+        string item = (string)stack.Pop();
+        Console.WriteLine($"Item: {item}");
+    }
+}
--- WiredBrainCoffe.StackApp/SimpleStack.cs
+++ WiredBrainCoffe.StackApp/SimpleStack.cs
@@ -1,9 +1,9 @@
-﻿public class SimpleStack
+﻿public class SimpleStackDouble
 {
     private readonly double[] _items;
     private int _currentIndex = -1;
 
-    public  SimpleStack() =>   _items = new double[10];
+    public  SimpleStackDouble() =>   _items = new double[10];
 
     public int Count => _currentIndex + 1;
 
@@ -12,4 +12,20 @@
     public double Pop() => _items[_currentIndex--];
 
     
+}
+
+public class SimpleStackString
+{
+    private readonly string[] _items;
+    private int _currentIndex = -1;
+
+    public SimpleStackString() => _items = new string[10];
+
+    public int Count => _currentIndex + 1;
+
+    public void Push(string item) => _items[++_currentIndex] = item;
+
+    public string Pop() => _items[_currentIndex--];
+
+
 }
